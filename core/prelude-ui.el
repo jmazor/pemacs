b28580498347e4feb321c5a233c2067816889c16
molezs <mazorjustin@gmail.com>
2020-10-26 22:04:43 -0400

    removed line numbers

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -1,6 +1,5 @@
 ;; prelude-ui.el My custom Emacs UI config
-;; Please note that I use global-display-line-numbers-mode, however I recompile emacs so that there is no  margin on the line numbers
-;; If anyone comes accross this I plan on creating a package that does this, however I am not yet informed enough. I had issues with nlinum and did not like linum. Im not picky though.
+;; TODO redo modline bar. Will display line number there
 
 ;; remove Toolbar
 (tool-bar-mode -1)
@@ -17,10 +16,7 @@
 ;; disable splash screen
 (setq inhibit-splash-screen t)
 
-;; Line numbers in Graphic mode and remove in term
-;; FIXME Fix line numbers
-(when (display-graphic-p)
-  (global-display-line-numbers-mode t))
+
 (unless (display-graphic-p)
   ;; adds a margin in the terminal
   (add-hook 'window-configuration-change-hook
