42e8fcaa828cf5b2150abe960adc70b75f3e5077
Justin Mazor <65927739+molezs@users.noreply.github.com>
2020-10-29 14:14:11 -0400

    testing line numbers

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -21,6 +21,32 @@
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
 
+
+;; Show native line numbers if possible, otherwise use `linum'
+(if (fboundp 'display-line-numbers-mode)
+    (use-package display-line-numbers
+      :ensure nil
+      :hook (prog-mode . display-line-numbers-mode))
+  (use-package linum-off
+    :demand
+    :defines linum-format
+    :hook (after-init . global-linum-mode)
+    :init (setq linum-format "%4d ")
+    :config
+    ;; Highlight current line number
+
+;; Suppress GUI features
+(setq use-file-dialog nil
+      use-dialog-box nil
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message t)
+
+;; Display dividers between windows
+(setq window-divider-default-places t
+      window-divider-default-bottom-width 1
+      window-divider-default-right-width 1)
+(add-hook 'window-setup-hook #'window-divider-mode)
+
 ;; We deal with doom-modeline here instead of modules
 ;; This looks really nice so I will be keeping it and modifiying it as long as it doesnt slow me down
 ;; Its seems fast however
