50a140f2ec96dd7563adb0751abf15e02ddad764
molezs <mazorjustin@gmail.com>
2020-11-05 16:16:40 -0500

    Removed obselete dir function

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -135,8 +135,4 @@
 ;; undo tree
 (global-undo-tree-mode)
 
-;; Z unzips files
-(eval-after-load "dired-aux"
-   '(add-to-list 'dired-compress-file-suffixes 
-                 '("\\.zip\\'" ".zip" "unzip")))
 (provide 'prelude-editor)
