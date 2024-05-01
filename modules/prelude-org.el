33f30388dccb01faf73e2e5990be8cce398a788d
jmazor <jmazor.noreply.github.com>
2024-05-01 01:41:48 -0400

    Deleted deprecated files

--- modules/prelude-org.el
+++
@@ -1,3 +0,0 @@
-(add-hook 'org-mode-hook (lambda () (hl-todo-mode 1)))
-
-(provide 'prelude-org)
--- modules/prelude-yasnippet.el
+++
@@ -1,7 +0,0 @@
-;;; prelude-yasnippet.el
-
-;; not part of prelude
-
-(require 'yasnippet)
-(yas-global-mode 1)
-(provide 'prelude-yasnippet)
